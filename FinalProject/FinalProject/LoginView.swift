0df64b601c440e37e5883df289e32d249fd5493d
Jonathan Pang <pangj@bxscience.edu>
2021-01-18 11:47:25 -0500

    Final Final Product

--- FinalProject/FinalProject/LoginView.swift
+++ FinalProject/FinalProject/LoginView.swift
@@ -15,110 +15,113 @@ struct LoginView: View {
     @State var secure   = true
     
     var body: some View {
-        VStack(alignment: .center, spacing: UIScreen.main.bounds.height / 512 * 3) {
-            ZStack{
-                customButton("", width: UIScreen.main.bounds.width / 8 * 7, height: UIScreen.main.bounds.height / 8 * 2, color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                VStack {
-                    //username Field
-                    HStack {
-                        Text("Email:")
-                        Spacer()
-                    }
-                    .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 48)
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 25)
-                            .stroke(Color.black, lineWidth: 1)
-                            .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 24)
-                        TextField("Email: ", text: $email)
-                            .autocapitalization(.none)
-                            .font(.system(size: 16))
-                            .multilineTextAlignment(.leading)
-                            .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 24)
-                    }
-                    
-                    //password Field
-                    HStack {
-                        Text("Password:")
-                        Spacer()
-                    }
-                    .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 48)
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 25)
-                            .stroke(Color.black, lineWidth: 1)
-                            .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 24)
+        NavigationView {
+            VStack(alignment: .center, spacing: UIScreen.main.bounds.height / 512 * 3) {
+                ZStack{
+                    customButton("", width: UIScreen.main.bounds.width / 8 * 7, height: UIScreen.main.bounds.height / 8 * 2, color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                    VStack {
+                        //username Field
                         HStack {
-                            if secure {
-                                SecureField("Password: ", text: $password)
-                                    .autocapitalization(.none)
-                                    .font(.system(size: 16))
-                                    .multilineTextAlignment(.leading)
-                                    .frame(width: UIScreen.main.bounds.width / 64 * 33, height: UIScreen.main.bounds.height / 24)
-                                Spacer()
-                                Image(systemName: "eye.fill")
-                                    .foregroundColor(Color.black)
-                                    .onTapGesture {
-                                        withAnimation {
-                                            secure.toggle()
-                                        }
-                                    }
-                            }
-                            else {
-                                TextField("Password: ", text: $password)
-                                    .autocapitalization(.none)
-                                    .font(.system(size: 16))
-                                    .multilineTextAlignment(.leading)
-                                    .frame(width: UIScreen.main.bounds.width / 16 * 9, height: UIScreen.main.bounds.height / 24)
-                                Spacer()
-                                Image(systemName: "eye.slash.fill")
-                                    .foregroundColor(Color.black)
-                                    .onTapGesture {
-                                        withAnimation {
-                                            secure.toggle()
-                                        }
-                                    }
-                            }
+                            Text("Email:")
+                            Spacer()
                         }
-                        .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 24)
+                        .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 48)
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(Color.black, lineWidth: 1)
+                                .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 24)
+                            TextField("Email: ", text: $email)
+                                .autocapitalization(.none)
+                                .font(.system(size: 16))
+                                .multilineTextAlignment(.leading)
+                                .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 24)
+                        }
+                        
+                        //password Field
+                        HStack {
+                            Text("Password:")
+                            Spacer()
+                        }
+                        .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 48)
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(Color.black, lineWidth: 1)
+                                .frame(width: UIScreen.main.bounds.width / 16 * 12, height: UIScreen.main.bounds.height / 24)
+                            HStack {
+                                if secure {
+                                    SecureField("Password: ", text: $password)
+                                        .autocapitalization(.none)
+                                        .font(.system(size: 16))
+                                        .multilineTextAlignment(.leading)
+                                        .frame(width: UIScreen.main.bounds.width / 64 * 33, height: UIScreen.main.bounds.height / 24)
+                                    Spacer()
+                                    Image(systemName: "eye.fill")
+                                        .foregroundColor(Color.black)
+                                        .onTapGesture {
+                                            withAnimation {
+                                                secure.toggle()
+                                            }
+                                        }
+                                }
+                                else {
+                                    TextField("Password: ", text: $password)
+                                        .autocapitalization(.none)
+                                        .font(.system(size: 16))
+                                        .multilineTextAlignment(.leading)
+                                        .frame(width: UIScreen.main.bounds.width / 16 * 9, height: UIScreen.main.bounds.height / 24)
+                                    Spacer()
+                                    Image(systemName: "eye.slash.fill")
+                                        .foregroundColor(Color.black)
+                                        .onTapGesture {
+                                            withAnimation {
+                                                secure.toggle()
+                                            }
+                                        }
+                                }
+                            }
+                            .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 24)
+                        }
+                        
                     }
-                    
                 }
-            }
-            
-            
-            // HStack for the user's next steps
-            HStack {
-                // attempts to login
-                skyBlueButton("Login", width: UIScreen.main.bounds.width / 4, height: UIScreen.main.bounds.height / 36)
-                    .onTapGesture {
-                        // login
-                        login1(email: email, password: password) { (result) in
-                            user.JWT = result.token
-                            user.username = result.user.username
-                            user.isAdmin  = result.user.isAdmin
-                            user.userID   = result.user.id
-                            if(user.isAdmin) {
-                                screen.currentScreen = 4
-                            }
-                            else {
-                                screen.currentScreen = 2
+                
+                
+                // HStack for the user's next steps
+                HStack {
+                    // attempts to login
+                    skyBlueButton("Login", width: UIScreen.main.bounds.width / 4, height: UIScreen.main.bounds.height / 36)
+                        .onTapGesture {
+                            // login
+                            login1(email: email, password: password) { (result) in
+                                user.JWT = result.token
+                                user.username = result.user.username
+                                user.isAdmin  = result.user.isAdmin
+                                user.userID   = result.user.id
+                                if(user.isAdmin) {
+                                    screen.currentScreen = 4
+                                }
+                                else {
+                                    screen.currentScreen = 2
+                                }
                             }
                         }
-                    }
+                }
+                .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 30)
+                
+                // HStack to register an account
+                HStack {
+                    Spacer()
+                    customButton("Register an account", width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height / 36, color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .font(.system(size: 14))
+                        .foregroundColor(Color.black)
+                        .onTapGesture {
+                            screen.currentScreen = 1
+                        }
+                    Spacer()
+                }
+                .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 30)
             }
-            .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 30)
-            
-            // HStack to register an account
-            HStack {
-                Spacer()
-                customButton("Register an account", width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height / 36, color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                    .font(.system(size: 14))
-                    .foregroundColor(Color.black)
-                    .onTapGesture {
-                        screen.currentScreen = 1
-                    }
-                Spacer()
-            }
-            .frame(width: UIScreen.main.bounds.width / 16 * 11, height: UIScreen.main.bounds.height / 30)
+            .navigationTitle("HKP Shop")
         }
     }
 }
